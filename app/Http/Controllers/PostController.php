0d290f6d3fcd6a99b71995a9626e144018f91147
Raul Escobar <escobarnet182@hotmail.com>
2021-03-06 13:36:17 -0500

    Listado de blogs

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+
+class PostController extends Controller
+{
+    public function index(){
+        $posts = Post::where('status',2)->get();
+
+        return view('posts.index',compact('posts'));
+    }
+}
