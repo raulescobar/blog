4667401411b80b714077594ae359e5116b0a39ef
Raul Escobar <escobarnet182@hotmail.com>
2021-03-07 14:53:23 -0500

    Filtrar post por categorias

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -24,4 +25,13 @@ class PostController extends Controller
 
         return view('posts.show',compact('post','similares'));
     }
+
+    public function category(Category $category){
+        $posts = Post::where('category_id',$category->id)
+                            ->where('status',2)
+                            ->latest('id')
+                            ->paginate(4);
+        return view('posts.category',compact('posts','category'));
+    }
+
 }
